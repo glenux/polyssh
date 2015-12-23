54e2d3195c25ba5f074cdf40f94686d3b6f16b93
Glenn Y. Rolland <glenux@glenux.net>
2015-12-24 00:31:11 +0100

    Make verbose.

--- lib/polyssh/command_builder.rb
+++ lib/polyssh/command_builder.rb
@@ -8,14 +8,16 @@ module PolySSH
     end
 
     def visit_polyssh_nodelist node_list
-        puts "Visiting NodeList" + node_list.inspect
+        puts "Visiting NodeList"
+        pp node_list
         if node_list.count > 0 then
           node_list.first.accept(self)
         end
     end
 
     def visit_polyssh_nodeentry node_entry
-      puts "Visiting NodeEntry" + node_entry.inspect
+      puts "Visiting NodeEntry"
+      pp node_entry
       unless node_entry.next.nil? then
         node_entry.next.accept(self)
       end
