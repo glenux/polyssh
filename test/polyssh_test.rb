9c3b759572fd4d20e06285c37bec5feda419d249
Glenn Y. Rolland <glenux@glenux.net>
2016-01-03 20:26:16 +0100

    Fix spec.

--- test/polyssh_test.rb
+++ test/polyssh_test.rb
@@ -1,11 +1,11 @@
 require 'test_helper'
 
-class MsshTest < Minitest::Test
+class PolySSHTest < Minitest::Test
   def test_that_it_has_a_version_number
-    refute_nil ::Mssh::VERSION
+    refute_nil ::PolySSH::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
-  end
+  #def test_it_does_something_useful
+  #  assert false
+  #end
 end
